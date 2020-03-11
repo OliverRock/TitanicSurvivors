e2a1e53bba2569f1f4d322cf07d7f7641b8a2599
oliverrock <ollie.rock@gmail.com>
2020-03-11 21:29:12 +0000

    Euclidean distance

---
+++ myKNN/testing.py
@@ -0,0 +1,10 @@
+from math import sqrt
+
+
+def euclidean_distance(row1, row2):
+    distance = 0.0
+    for i in range(len(row1) -1 ):
+        distance += (row1[i] - row2[i]) ** 2
+    return sqrt(distance)
+
+
